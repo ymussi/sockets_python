b6d73dee3ad62ec651e57c4ed4506c194d8feb8a
Yuri Mussi <mussi@MacBook-Air-de-Yuri.local>
2019-09-02 18:22:32 -0300

    changes

--- client.py
+++ client.py
@@ -1,11 +1,11 @@
 import socket
 
-target_host = 'localhost'
-target_port = 7000
+target_host = 'localhost' # IP do servidor tcp.
+target_port = 7000 # Porta do servidor tcp.
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host, target_port))
-msg = input('Envie uma MSG ao SERVIDOR: ')
+msg = input('Envie uma MSG ao SERVIDOR: ') # Mensagem a ser enviada para o servidor.
 client.send(msg.encode('utf-8'))
-response = client.recv(4096)
+response = client.recv(4096) # Resposta do Servidor.
 print(response)
--- server.py
+++ server.py
@@ -1,19 +1,19 @@
 import socket
 import threading
 
-bind_ip = 'localhost'
-bind_port = 7000
+bind_ip = 'localhost' # IP do Servidor TCP
+bind_port = 7000 # Porta do Servidor TCP
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((bind_ip, bind_port))
-server.listen(5)
+server.listen(5) # Limite de conexoes com o servidor
 print(f'[*] Escutando {bind_ip}:{bind_port}')
 
 def handle_client(client_socket):
     request = client_socket.recv(1024)
-    print(f'- [*] Recebido: {request.decode()}')
+    print(f'- [*] Recebido: {request.decode()}') # Mensagem recebida do cliente.
     print('-' * 30)
-    msg1 = f'Mensagem destinada do cliente: {addr[0]}'
+    msg1 = f'Mensagem do cliente: {addr[0]}'
     msg2 = f' ACK! - Recebido pelo servidor.'
     client_socket.send(msg1.encode('utf-8'))
     client_socket.send(msg2.encode('utf-8'))
@@ -24,7 +24,4 @@ while True:
     print(f'Conexão aceita de: {addr[0]}, {addr[1]}')
     client_handler = threading.Thread(target=handle_client, args=(client,))
     client_handler.start()
-
-    
-
 
